5aa3876d30bc9073bd290359727c78ef45858b35
HyeopG <87799990+HyeopG@users.noreply.github.com>
2021-12-27 19:28:18 +0900

    Delta Update (경사하강법)

--- Multy-Perceptron.py
+++ Multy-Perceptron.py
@@ -9,12 +9,12 @@ class Multi_Perceptron:
         self.target = [0, 1, 1, 0]      # 목표값
         self.loss = [[0.0, 0.0], [0.0]]
         self.result = [[0.0, 0.0], [0.0]]
-        self.w1 = [[round(random.random(), 1), round(random.random(), 1)], [round(random.random(), 1), round(random.random(), 1)]]    # input Weight
-        self.w2 = [round(random.random(), 1), round(random.random(), 1)]    # hidden layer Weight
+        self.w1 = [[round(random.random(), 1) - 0.5, round(random.random(), 1) - 0.5], [round(random.random() - 0.5, 1), round(random.random() - 0.5, 1)]]    # input Weight
+        self.w2 = [round(random.random() - 0.5, 1), round(random.random() - 0.5, 1)]    # hidden layer Weight
         self.b = 1     # bias는 항상 1
-        self.bw = [[round(random.random(), 1), round(random.random(), 1)], [round(random.random(), 1)]]    # bias Weight
+        self.bw = [[round(random.random() - 0.5, 1), round(random.random() - 0.5, 1)], [round(random.random() - 0.5, 1)]]    # bias Weight
         self.running = 0.1  # 이동 값
-        self.epoch = 1000   # 반복 횟수
+        self.epoch = 10000   # 반복 횟수
         self.tss = 0
         self.Run() # 시작.
 
@@ -28,14 +28,16 @@ class Multi_Perceptron:
     def Sigmoid(self, result):
         return 1 / (1+np.exp(-result))
 
+    def D_Sigmoid(self, loss, sig):
+        return loss * sig * (1-sig)
+
     def WeightedSum(self, x, layer, number=1):    # layer = 층, number = 알파(?) 갯수
         temp_x = [x[0], x[1]]
         w = []
-        number -= 1
         # 웨이트 넣기.
         if layer == 1:
-            for i in range(len(self.w1[number])):        # 받는 Weight 갯수
-                w.append(self.w1[number][i])
+            for i in range(len(self.w1[number-1])):        # 받는 Weight 갯수
+                w.append(self.w1[number-1][i])
         elif layer == 2:
             for i in range(len(self.w2)):
                 w.append(self.w2[i])
@@ -46,14 +48,14 @@ class Multi_Perceptron:
         temp_x.append(self.b)
         np_x = np.array(temp_x)
 
-        w.append(self.bw[layer-1][number])
+        w.append(self.bw[layer-1][number-1])
         np_w = np.array(w)
 
-        result = np.sum(np_w * np_x)
+        result = np.sum(np_x * np_w)
 
         return result
 
-    def Learning(self, x):
+    def Exam(self, x):
 
         # 1층 연산
         self.result[0][0] = self.Sigmoid(self.WeightedSum(x, 1, 1))
@@ -71,10 +73,14 @@ class Multi_Perceptron:
         self.loss[1][0] = target - self.result[1][0]
 
         # 업데이트를 먼저하고 밑에 수정.
-        self.WeightedUpdate([self.result[0][0], self.result[0][1]], self.loss[1][0], 2)      # 2층 업데이트
+        result_list = [self.result[0][0], self.result[0][1]]         # 1층 연산 끝낸 값 모음
+        self.WeightedUpdate(result_list, self.loss[1][0], 2)         # 2층 업데이트
 
-        self.loss[0][0] = self.loss[1][0] * self.w2[0]
-        self.loss[0][1] = self.loss[1][0] * self.w2[1]
+        # self.loss[0][0] = self.loss[1][0] * self.w2[0]
+        # self.loss[0][1] = self.loss[1][0] * self.w2[1]
+
+        self.loss[0][0] = self.D_Sigmoid(self.loss[1][0], self.result[0][0])
+        self.loss[0][1] = self.D_Sigmoid(self.loss[1][0], self.result[0][1])
 
         self.WeightedUpdate(x, self.loss[0][0], 1, 1)
         self.WeightedUpdate(x, self.loss[0][1], 1, 2)
@@ -84,11 +90,10 @@ class Multi_Perceptron:
         print("pss :", pss)
 
     def WeightedUpdate(self, x, loss, layer, number=1):
-        number -= 1
         # Weight Update
         if layer == 1:
-            for i in range(len(self.w1[number])):        # 받는 Weight 갯수
-                self.w1[number][i] += self.running * loss * x[i]
+            for i in range(len(self.w1[number-1])):        # 받는 Weight 갯수
+                self.w1[number-1][i] += self.running * loss * x[i]
         elif layer == 2:
             for i in range(len(self.w2)):
                 self.w2[i] += self.running * loss * x[i]
@@ -96,18 +101,17 @@ class Multi_Perceptron:
             print("error : layer 입력 에러")
             return
 
-        self.bw[layer-1][number] += self.running * loss * self.b
+        self.bw[layer-1][number-1] += self.running * loss * self.b
 
     def Run(self):
         for epoch in range(self.epoch):
             self.tss = 0
             count = 0
             for x1, x2 in self.x:
-                self.Learning([x1, x2])
+                self.Exam([x1, x2])
                 self.Backpropagation([x1, x2], self.target[count])
                 count += 1
-            print("weight :", self.w1[0], self.w1[1], self.w2)
-            print("bias :", self.bw[0], self.bw[1])
+
             self.Show()
             print("epoch :", epoch, "tss :", self.tss / count, "\n")
 
@@ -120,7 +124,7 @@ class Multi_Perceptron:
             count = 0
             for x1, x2 in self.x:
                 print("(", x1, ",", x2, ")", end="")
-                print("target :", self.target[count], "output :", self.Learning([x1, x2]), "th(output) :", self.Threshold(self.Learning([x1, x2])))
+                print("target :", self.target[count], "output :", self.Exam([x1, x2]), "th(output) :", self.Threshold(self.Exam([x1, x2])))
                 count += 1
         except:
             print("Error : 입력 에러 발생.")
